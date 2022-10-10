7a094e9ea630d1c99604369ed0039afd567d875a
Marco Burrometo <marco.burrometo@gmail.com>
2022-10-10 15:50:19 +0200

    publish

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import { processColor } from "react-native";
 
 const RNLinearGradientView =
-  require(".src//LinearGradientViewNativeComponent").default;
+  require("./src/LinearGradientViewNativeComponent").default;
 
 export const LinearGradient = ({ colors, ...rest }) => {
   return <RNLinearGradientView {...rest} colors={colors.map(processColor)} />;
