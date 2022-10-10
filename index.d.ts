9ef144bff3231b8aa36ae44d2bc01add4d48d864
Marco Burrometo <marco.burrometo@gmail.com>
2022-10-10 15:25:23 +0200

    types

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,23 @@
 import { Component } from "react";
+import { ViewProps } from "react-native";
 
 export type Point = $ReadOnly<{ x: Float; y: Float }>;
 
-export interface LinearGradientProps {
+export interface LinearGradientProps extends ViewProps {
   start?: Point;
   end?: Point;
-  colors: Array<ColorValue>;
+  colors: Array<string>;
   locations?: Array<Float>;
   useAngle?: boolean;
   angleCenter?: Point;
   angle?: Float;
 }
-
-export const LinearGradient: Component<LinearGradientProps>;
+export default class LinearGradient extends Component<
+  LinearGradientProps,
+  any
+> {
+  static defaultProps: {
+    colors: string[];
+  };
+  render(): JSX.Element;
+}
