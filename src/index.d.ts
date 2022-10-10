f632fb7b94e1d6044d2a98e80e06200221a41bb3
Marco Burrometo <marco.burrometo@gmail.com>
2022-10-10 15:15:25 +0200

    types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,14 @@
-declare module 'LinearGradient' {
-    import { Component } from 'react';
-    export interface LinearGradientProps {
-        colors: string[];
-    }
-    export default class LinearGradient extends Component<LinearGradientProps, any> {
-        static defaultProps: {
-            colors: string[];
-        };
-        render(): JSX.Element;
-    }
+import { Component } from "react";
+
+export interface LinearGradientProps {
+  colors: string[];
+}
+export default class LinearGradient extends Component<
+  LinearGradientProps,
+  any
+> {
+  static defaultProps: {
+    colors: string[];
+  };
+  render(): JSX.Element;
 }
